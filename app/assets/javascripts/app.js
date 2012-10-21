6f518b4a416edda53eb25106e6b4ab046bed563f
galettos <scottgaletto@gmail.com>
2012-10-21 18:36:20 -0500

    initial commit

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,24 +9,24 @@ window.onload = function() {
 
 	var answers = [
 		'It is certain',
-		'It is decidedly so',
-		'Without a doubt',
-		'Yes, definitely',
-		'You may rely on it',
-		'As I see it, yes',
-		'Most likely',
-		'Outlook good',
-		'Yes',
+		// 'It is decidedly so',
+		// 'Without a doubt',
+		// 'Yes, definitely',
+		// 'You may rely on it',
+		// 'As I see it, yes',
+		// 'Most likely',
+		// 'Outlook good',
+		// 'Yes',
 		'Signs point to yes',
 		'Reply hazy, try again',
-		'Ask again later',
-		'Better not tell you now',
-		'Cannot predict now',
+		// 'Ask again later',
+		// 'Better not tell you now',
+		// 'Cannot predict now',
 		'Concentrate and ask again',
-		'Don\'t count on it',
+		// 'Don\'t count on it',
 		'My reply is no',
 		'My sources say no',
-		'Outlook not so good',
+		// 'Outlook not so good',
 		'Very doubtful'
 	];
 	var answer = answers[Math.floor(Math.random() * (answers.length))];
@@ -61,7 +61,7 @@ window.onload = function() {
 		
 		
 		// a device is place on a flat surface
-		isStopped = (beta < 5 && beta > -5 && gamma < 5 && gamma > -5) ? true : false;
+		isStopped = (beta < 10 && beta > -10 && gamma < 10 && gamma > -10) ? true : false;
 		
 		if(isStopped == false) {
 			message.style.opacity = .75;
