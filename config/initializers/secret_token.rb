9640a21ce783cd9e0889805ee21691a3a1aef99b
galettos <scottgaletto@gmail.com>
2012-10-21 17:09:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MsgLab8ball::Application.config.secret_token = 'e52fdede14cfa3c49495718bcf6bf2e80a0bc7d72873f395bc0a380ac04fcbf07ede2f606dbd6262ea2d7fb9f011b7c8d34db55ce4ddb107faaf22fd3a91ee8d'
